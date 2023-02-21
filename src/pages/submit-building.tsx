9e67dd7f52effa2b6db95495e5a310eda17bbd3a
yaronlavi1992 <yaronlavi1992@gmail.com>
2023-02-22 00:07:03 +0200

    lets go

--- src/pages/submit-building.tsx
+++ src/pages/submit-building.tsx
@@ -13,7 +13,6 @@ const SubmitBuildingPage: NextPage = () => {
 
   async function handleSubmitBuildingSpecification(e: React.FormEvent) {
     e.preventDefault();
-    // eslint-disable-next-line promise/catch-or-return
     await createBuildingMutation.mutateAsync({
       energyGrade,
       description
